e39cb8bd0e8cab952fe89b8c8dd7b96bd10b7126
Alex Marin <alex.ukf@gmail.com>
2012-05-11 19:04:53 +0300

    adaugat proiect EasyServerClient

--- EasyFilterClient/src/EasyFilterClient/EasyFilterClient.java
+++ EasyFilterClient/src/EasyFilterClient/EasyFilterClient.java
@@ -2,13 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package easyfilter;
+package EasyFilterClient;
 
 /**
  *
  * @author TEO
  */
-public class EasyFilter {
+public class EasyFilterClient {
 
     /**
      * @param args the command line arguments
--- EasyFilterClient/src/EasyFilterClient/EasyImageReader.java
+++ EasyFilterClient/src/EasyFilterClient/EasyImageReader.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package easyfilter;
+package EasyFilterClient;
 
 import java.io.File;
 import java.io.FileNotFoundException;
