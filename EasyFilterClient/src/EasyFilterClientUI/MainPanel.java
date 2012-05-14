fb6632ef4e73579d95521f260d74f3300b754082
unknown <BOGDAN@BOGDAN-PC.(none)>
2012-05-15 01:42:48 +0300

    interfata grafica de baza

---
+++ EasyFilterClient/src/EasyFilterClientUI/MainPanel.java
@@ -0,0 +1,130 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EasyFilterClientUI;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author BOGDAN
+ */
+public class MainPanel extends JPanel{
+    
+    private JTextField filename;
+    private JTextField directory;
+    private JButton upload;
+    private JFileChooser fileChooser;
+    private JTextField downloadFile;
+    private JButton download;
+    private JPanel northPanel;
+    private JPanel southPanel;
+    
+    
+    public MainPanel(){
+        setLayout(new GridLayout(2, 1));
+        northPanel = new JPanel();
+        
+        upload = new JButton("Upload");
+        filename = new JTextField("",20);
+        directory = new JTextField();
+        upload.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileChooser = new JFileChooser(System.getProperty("user.dir"));
+                int rVal = fileChooser.showOpenDialog(MainPanel.this);
+                if (rVal == JFileChooser.APPROVE_OPTION) {
+                    filename.setText(System.getProperty("user.dir")+"\\"+fileChooser.getSelectedFile().getName());
+                    directory.setText(fileChooser.getCurrentDirectory().toString());
+                }
+                if (rVal == JFileChooser.CANCEL_OPTION) {
+                    filename.setText("You pressed cancel");
+                    directory.setText("");
+                }
+            }
+        });
+        northPanel.add(upload);
+        northPanel.add(filename);
+        add(northPanel);
+        
+        southPanel = new JPanel(new GridBagLayout());
+        downloadFile = new JTextField("",20);
+        download = new JButton("Download");
+        southPanel.add(download);
+        southPanel.add(downloadFile);
+        add(southPanel);
+        
+    }
+
+    public JTextField getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(JTextField directory) {
+        this.directory = directory;
+    }
+
+    public JButton getDownload() {
+        return download;
+    }
+
+    public void setDownload(JButton download) {
+        this.download = download;
+    }
+
+    public JTextField getDownloadFile() {
+        return downloadFile;
+    }
+
+    public void setDownloadFile(JTextField downloadFile) {
+        this.downloadFile = downloadFile;
+    }
+
+    public JFileChooser getFileChooser() {
+        return fileChooser;
+    }
+
+    public void setFileChooser(JFileChooser fileChooser) {
+        this.fileChooser = fileChooser;
+    }
+
+    public JTextField getFilename() {
+        return filename;
+    }
+
+    public void setFilename(JTextField filename) {
+        this.filename = filename;
+    }
+
+    public JPanel getNorthPanel() {
+        return northPanel;
+    }
+
+    public void setNorthPanel(JPanel northPanel) {
+        this.northPanel = northPanel;
+    }
+
+    public JPanel getSouthPanel() {
+        return southPanel;
+    }
+
+    public void setSouthPanel(JPanel southPanel) {
+        this.southPanel = southPanel;
+    }
+
+    public JButton getUpload() {
+        return upload;
+    }
+
+    public void setUpload(JButton upload) {
+        this.upload = upload;
+    }
+    
+}
