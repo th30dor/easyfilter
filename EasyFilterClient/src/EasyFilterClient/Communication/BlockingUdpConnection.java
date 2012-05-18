082457f6b5ff3a53a758c50845ee9ff6cafd966b
Teo <gherghescu_teo@yahoo.com>
2012-05-19 01:23:11 +0300

    UDP functional fara download

--- EasyFilterClient/src/EasyFilterClient/Communication/BlockingUdpConnection.java
+++ EasyFilterClient/src/EasyFilterClient/Communication/BlockingUdpConnection.java
@@ -36,10 +36,10 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
      */
     private String serverIP;
     private int serverPort;
+    private int clientPort;
 
     /**
-     * Constructor
-     * Remembers the server address
+     * Constructor Remembers the server address
      */
     public BlockingUdpConnection() {
         EasyPropertiesReader epr = new EasyPropertiesReader("config/config.ini");
@@ -55,6 +55,7 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
         try {
             // Open server socket
             this.setUdpSocket(new DatagramSocket());
+            this.setClientPort(this.getUdpSocket().getLocalPort());
         } catch (IOException ex) {
             Logger.getLogger(BlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -69,7 +70,7 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
     public void sendRequest(common.Package pkg) {
         ObjectOutputStream oos = null;
         int i, j, numberOfSentPackets, chunkSize, tmp;
-        boolean notFinishedSendingImage = false;
+        boolean notFinishedSendingImage = true;
         byte[] packageBytes = null;
         ByteBuffer buf;
         byte b;
@@ -100,68 +101,59 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
         // bytes 15-19          - the client port
         // bytes 20-21          - the chunk position
         // byte  22-23          - number of chunks for current client
-        numberOfSentPackets = (int) Math.ceil(packageBytes.length / 1000);
-        while (notFinishedSendingImage) {
-            for (i = 0; i < numberOfSentPackets; i++) {
-                // compute the chunk size
-                if (1000 * numberOfSentPackets == packageBytes.length) {
-                    chunkSize = 1000;
+        numberOfSentPackets = (int) Math.ceil((double) packageBytes.length / 14000);
+        for (i = 0; i < numberOfSentPackets; i++) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            // compute the chunk size
+            if (14000 * numberOfSentPackets == packageBytes.length) {
+                chunkSize = 14000;
+            } else {
+                if (i == numberOfSentPackets - 1) {
+                    chunkSize = packageBytes.length - (numberOfSentPackets - 1) * 14000;
                 } else {
-                    if (i == numberOfSentPackets - 1) {
-                        chunkSize = packageBytes.length - (numberOfSentPackets - 1) * 1000;
-                    } else {
-                        chunkSize = 1000;
-                    }
+                    chunkSize = 14000;
                 }
+            }
 
-                buf = ByteBuffer.allocate(chunkSize + 21);
-                b = 0;
-                // add server IP
-                for (j = 0; j < this.getServerIP().length(); j++) {
-                    buf.put((byte) this.getServerIP().charAt(j));
-                }
-                for (j = 0; j < 15 - this.getServerIP().length(); j++) {
-                    buf.put((byte) 0);
-                }
-                // add server port
-                port = this.getServerPort() + "";
-                for (j = 0; j < port.length(); j++) {
-                    buf.put((byte) port.charAt(j));
-                }
-                for (j = 0; j < 5 - port.length(); j++) {
-                    buf.put((byte) 0);
-                }
+            buf = ByteBuffer.allocate(chunkSize + 22);
+            b = 0;
+            // add server IP
+            for (j = 0; j < this.getServerIP().length(); j++) {
+                buf.put((byte) this.getServerIP().charAt(j));
+            }
+            for (j = 0; j < 15 - this.getServerIP().length(); j++) {
+                buf.put((byte) 0);
+            }
+            // add server port
 
-                // add chunk position
-                tmp = i >> 8;
-                b = (byte)(tmp);
-                buf.put(b);
-                tmp = i & 255;
-                b = (byte)(tmp);
-                buf.put(b);
-                
-                // add number of chunks
-                tmp = numberOfSentPackets >> 8;
-                b = (byte)(tmp);
-                buf.put(b);
-                tmp = numberOfSentPackets & 255;
-                b = (byte)(tmp);
-                buf.put(b);
+            port = this.getClientPort() + "";
+            for (j = 0; j < port.length(); j++) {
+                buf.put((byte) port.charAt(j));
+            }
+            for (j = 0; j < 5 - port.length(); j++) {
+                buf.put((byte) 0);
+            }
 
-                // add image content to packet
-                buf.put(ByteBuffer.wrap(packageBytes, i * 1000, chunkSize));
+            buf.put((byte) i);
+            buf.put((byte) numberOfSentPackets);
 
-                // create the actual packet
-                DatagramPacket packet = new DatagramPacket(buf.array(), buf.array().length);
-                try {
-                    // set the address for the packet
-                    packet.setAddress(InetAddress.getByName(this.getServerIP()));
-                    packet.setPort(this.getServerPort());
-                    // finally send the packet
-                    this.getUdpSocket().send(packet);
-                } catch (Exception ex) {
-                    Logger.getLogger(BlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
-                }
+            // add image content to packet
+            buf.put(ByteBuffer.wrap(packageBytes, i * 14000, chunkSize));
+            buf.flip();
+            // create the actual packet
+            DatagramPacket packet = new DatagramPacket(buf.array(), buf.array().length);
+            try {
+                // set the address for the packet
+                packet.setAddress(InetAddress.getByName(this.getServerIP()));
+                packet.setPort(this.getServerPort());
+                // finally send the packet
+                this.getUdpSocket().send(packet);
+            } catch (Exception ex) {
+                Logger.getLogger(BlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -175,45 +167,47 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
     public common.Package receiveRequest() {
         common.Package pkg = null;
 
-        int chunkPosition, numberOfChunks, i, j, imageSize;
+        int chunkPosition, numberOfChunks, i, j, k, imageSize;
         Vector<byte[]> chunk = null;
         byte[] image;
         ByteArrayInputStream baos = null;
         ObjectInputStream ois = null;
         boolean isFirstPackage = true;
+        byte[] tempByteArray;
+        int receivedChunks = 0;
 
         // the buffer for the packet to be sent
         // bytes 0-19        - not used
         // bytes 20-21       - the chunk position
         // byte  22-23       - number of chunks for current client
-        byte[] buf = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
         while (true) {
             try {
+                byte[] buf = new byte[14022];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
                 this.getUdpSocket().receive(packet);
 
-                // get the client address
                 chunkPosition = buf[20];
-                chunkPosition = chunkPosition << 8;
-                chunkPosition |= buf[21];
-                
-                numberOfChunks = buf[22];
-                numberOfChunks = chunkPosition << 8;
-                numberOfChunks |= buf[23];
-                
-                
+                numberOfChunks = buf[21];
+
                 if (isFirstPackage) {
                     chunk = new Vector<byte[]>(numberOfChunks);
+                    for (k = 0; k < numberOfChunks; k++) {
+                        tempByteArray = new byte[1];
+                        chunk.add(tempByteArray);
+                    }
                     isFirstPackage = false;
                 }
-
                 // add image chunks
                 chunk.setElementAt(buf, chunkPosition);
+                receivedChunks++;
                 // check for last chunk
-                if (numberOfChunks == chunk.size()) {
+                if (numberOfChunks == receivedChunks) {
                     // the image is complete
                     // get the image size
-                    imageSize = 1000 * (numberOfChunks - 1) + chunk.elementAt(numberOfChunks - 1).length;
+                    int lastChunkSize = chunk.elementAt(numberOfChunks - 1).length;
+                    imageSize = 14000 * (numberOfChunks - 1) + lastChunkSize;
                     image = new byte[imageSize];
 
                     // create the package and return it to the client
@@ -228,11 +222,10 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
                     ois = new ObjectInputStream(baos);
                     try {
                         pkg = (common.Package) ois.readObject();
+                        return pkg;
                     } catch (ClassNotFoundException ex) {
                         Logger.getLogger(BlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
                     }
-
-                    return pkg;
                 }
             } catch (IOException ex) {
                 Logger.getLogger(BlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
@@ -263,5 +256,13 @@ public class BlockingUdpConnection implements ClientCommunicationInterface {
 
     private void setServerPort(int serverPort) {
         this.serverPort = serverPort;
+    }
+
+    public int getClientPort() {
+        return clientPort;
+    }
+
+    private void setClientPort(int clientPort) {
+        this.clientPort = clientPort;
     }
 }
--- EasyFilterServer/src/EasyFilterServer/Communication/ClientBlockingUdpConnection.java
+++ EasyFilterServer/src/EasyFilterServer/Communication/ClientBlockingUdpConnection.java
@@ -17,6 +17,7 @@ import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.util.Hashtable;
 import java.util.Vector;
@@ -36,23 +37,30 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
      * Hash table for mapping images to clients
      */
     private static Hashtable<String, Vector<byte[]>> images;
+    private static Hashtable<String, Integer> clientImageChunkCounters;
     private String currentClientIP;
     private int currentClientPort;
 
     /**
-     * Opens a socket with the data read from the config file;
-     * Used to communicate with the client/other server instances
+     * Constructor
+     */
+    public ClientBlockingUdpConnection() {
+    }
+
+    /**
+     * Opens a socket with the data read from the config file; Used to
+     * communicate with the client/other server instances
      */
     @Override
     public void openConnection() {
+        ClientBlockingUdpConnection.setImages(new Hashtable<String, Vector<byte[]>>());
+        ClientBlockingUdpConnection.setClientImageChunkCounters(new Hashtable<String, Integer>());
         try {
             EasyPropertiesReader epr = new EasyPropertiesReader("config/config.ini");
-            ClientBlockingUdpConnection.setImages(new Hashtable<String, Vector<byte[]>>());
-
             // Open server socket
             ClientBlockingUdpConnection.setUdpSocket(
                     new DatagramSocket(Integer.parseInt(epr.readProperty("Address", "port"))));
-        } catch (IOException ex) {
+        } catch (SocketException ex) {
             Logger.getLogger(ClientBlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -107,56 +115,49 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
         // bytes 0-19           - not used
         // bytes 20-21          - the chunk position
         // byte  22-23          - number of chunks for current client
-        numberOfSentPackets = (int) Math.ceil(packageBytes.length / 1000);
-        while (notFinishedSendingImage) {
-            for (i = 0; i < numberOfSentPackets; i++) {
-                // compute the chunk size
-                if (1000 * numberOfSentPackets == packageBytes.length) {
-                    chunkSize = 1000;
+        numberOfSentPackets = (int) Math.ceil((double) packageBytes.length / 14000);
+        for (i = 0; i < numberOfSentPackets; i++) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ClientBlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            // compute the chunk size
+            if (14000 * numberOfSentPackets == packageBytes.length) {
+                chunkSize = 14000;
+            } else {
+                if (i == numberOfSentPackets - 1) {
+                    chunkSize = packageBytes.length - (numberOfSentPackets - 1) * 14000;
                 } else {
-                    if (i == numberOfSentPackets - 1) {
-                        chunkSize = packageBytes.length - (numberOfSentPackets - 1) * 1000;
-                    } else {
-                        chunkSize = 1000;
-                    }
+                    chunkSize = 14000;
                 }
+            }
 
-                buf = ByteBuffer.allocate(chunkSize + 21);
-                b = 0;
-                // add unused bytes
-                for (j = 0; j < 20; j++) {
-                    buf.put(b);
-                }
-                // add chunk position
-                tmp = i >> 8;
-                b = (byte)(tmp);
+            buf = ByteBuffer.allocate(chunkSize + 22);
+            b = 0;
+            // add unused bytes
+            for (j = 0; j < 20; j++) {
                 buf.put(b);
-                tmp = i & 255;
-                b = (byte)(tmp);
-                buf.put(b);
+            }
 
-                // add number of chunks
-                tmp = numberOfSentPackets >> 8;
-                b = (byte)(tmp);
-                buf.put(b);
-                tmp = numberOfSentPackets & 255;
-                b = (byte)(tmp);
-                buf.put(b);
+            buf.put((byte) i);
+            buf.put((byte) numberOfSentPackets);
 
-                // add image content to packet
-                buf.put(ByteBuffer.wrap(packageBytes, i * 1000, chunkSize));
+            // add image content to packet
+            buf.put(ByteBuffer.wrap(packageBytes, i * 14000, chunkSize));
+            buf.flip();
 
-                // create the actual packet
-                DatagramPacket packet = new DatagramPacket(buf.array(), buf.array().length);
-                try {
-                    // set the address for the packet
-                    packet.setAddress(InetAddress.getByName(this.getCurrentClientIP()));
-                    packet.setPort(this.getCurrentClientPort());
-                    // finally send the packet
-                    ClientBlockingUdpConnection.getUdpSocket().send(packet);
-                } catch (Exception ex) {
-                    Logger.getLogger(ClientBlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
-                }
+            // create the actual packet
+            DatagramPacket packet = new DatagramPacket(buf.array(), buf.array().length);
+            try {
+                // set the address for the packet
+                packet.setAddress(InetAddress.getByName(getCurrentClientIP()));
+                packet.setPort(this.getCurrentClientPort());
+                // finally send the packet
+                ClientBlockingUdpConnection.getUdpSocket().send(packet);
+            } catch (Exception ex) {
+                Logger.getLogger(ClientBlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -170,34 +171,31 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
     public Object receiveFile() {
         common.Package pkg = null;
         String clientIP, clientPort, key;
-        int chunkPosition, numberOfChunks, i, j, imageSize;
+        int chunkPosition, numberOfChunks, i, j, k, imageSize;
         Vector<byte[]> chunk;
         byte[] image;
         ByteArrayInputStream baos = null;
         ObjectInputStream ois = null;
+        byte[] tempByteArray;
 
         // the buffer for the received packet
-        // bytes 0-14  contain the client IP
-        // bytes 15-19       - the client port
-        // bytes 20-21       - the chunk position
-        // byte  22-23       - number of chunks for current client
-        byte[] buf = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        // bytes 0-14 contain the client IP
+        // bytes 15-19    - the client port
+        // bytes 20       - the chunk position
+        // byte  21       - number of chunks for current client
         while (true) {
             try {
+                byte[] buf = new byte[14022];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 ClientBlockingUdpConnection.getUdpSocket().receive(packet);
 
                 // get the client address
                 clientIP = new String(buf, 0, 15);
                 clientPort = new String(buf, 15, 5);
                 key = clientIP + ":" + clientPort;
-                chunkPosition = buf[20];
-                chunkPosition = chunkPosition << 8;
-                chunkPosition |= buf[21];
 
-                numberOfChunks = buf[22];
-                numberOfChunks = chunkPosition << 8;
-                numberOfChunks |= buf[23];
+                chunkPosition = buf[20];
+                numberOfChunks = buf[21];
 
                 // add image chunks to hash table
                 if (images.containsKey(key)) {
@@ -206,11 +204,12 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
                     // replace the old vector
                     images.put(key, chunk);
 
+                    clientImageChunkCounters.put(key, 1 + ClientBlockingUdpConnection.getClientImageChunkCounters().get(key));
                     // check for last chunk
-                    if (numberOfChunks == chunk.size()) {
+                    if (numberOfChunks - 1 == ClientBlockingUdpConnection.getClientImageChunkCounters().get(key)) {
                         // the image is complete
                         // get the image size
-                        imageSize = 1000 * (numberOfChunks - 1) + chunk.elementAt(numberOfChunks - 1).length;
+                        imageSize = 14000 * (numberOfChunks - 1) + chunk.elementAt(numberOfChunks - 1).length;
                         image = new byte[imageSize];
 
                         // create the package and return it to the client
@@ -233,12 +232,17 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
                         images.remove(key);
                         // set the current client address
                         this.setCurrentClientIP(clientIP);
-                        this.setCurrentClientPort(Integer.parseInt(clientPort));
+                        this.setCurrentClientPort(Integer.parseInt(clientPort.trim()));
 
                         return pkg;
                     }
                 } else {
                     chunk = new Vector<byte[]>(numberOfChunks);
+                    for (k = 0; k < numberOfChunks; k++) {
+                        tempByteArray = new byte[1];
+                        chunk.add(tempByteArray);
+                    }
+                    clientImageChunkCounters.put(key, 0);
                     chunk.setElementAt(buf, chunkPosition);
                     images.put(key, chunk);
                 }
@@ -253,11 +257,13 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
      */
     @Override
     public void closeConnection() {
-        try {
-            this.getUdpSocket().close();
-        } catch (Exception ex) {
-            Logger.getLogger(ClientBlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        try {
+//            ClientBlockingUdpConnection.getUdpSocket().disconnect();
+//            ClientBlockingUdpConnection.getUdpSocket().close();
+//
+//        } catch (Exception ex) {
+//            Logger.getLogger(ClientBlockingUdpConnection.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 
     // ~~~~~~~~ Getters and Setters ~~~~~~~~~~
@@ -291,5 +297,13 @@ public class ClientBlockingUdpConnection implements CommunicationInterface {
 
     public void setCurrentClientPort(int currentClientPort) {
         this.currentClientPort = currentClientPort;
+    }
+
+    public static Hashtable<String, Integer> getClientImageChunkCounters() {
+        return clientImageChunkCounters;
+    }
+
+    private static void setClientImageChunkCounters(Hashtable<String, Integer> clientImageChunkCounters) {
+        ClientBlockingUdpConnection.clientImageChunkCounters = clientImageChunkCounters;
     }
 }
